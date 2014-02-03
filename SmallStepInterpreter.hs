2532fb7c4b4c81b683b1010a9157c71b5af4d27c
Podkopaev Anton <podkoav239@gmail.com>
2014-02-03 22:38:44 +0400

    Added counter to SS CBN.

--- SmallStepInterpreter.hs
+++ SmallStepInterpreter.hs
@@ -1,4 +1,4 @@
-module SmallStepInterpreter (int) where
+module SmallStepInterpreter (int, intC) where
 
 import Data
 import DataUtil
@@ -21,3 +21,31 @@ intStep p (GCall gname (Ctr cname cargs : args)) =
 
 intStep p (GCall gname (e:es)) =
 	(GCall gname (intStep p e : es))
+
+------------------------------
+
+updateState :: (Expr -> (Expr, Integer)) -> (Expr, Integer) -> (Expr, Integer)
+updateState f (e, n) =
+  (e', n + n') where
+    (e', n') = f e
+
+intC :: Program -> Expr -> (Expr, Integer)
+intC p e = until (isValue . fst) (updateState (intStepC p)) (e, 0)
+
+intStepC :: Program -> Expr -> (Expr, Integer)
+intStepC p (Ctr name args) =
+  let (values, x:xs) = span isValue args
+      (steppedX, c)  = intStepC p x
+  in (Ctr name (values ++ (steppedX : xs)), c)
+
+intStepC p (FCall name args) =
+	(body // zip vs args, 1) where
+		(FDef _ vs body) = fDef p name
+
+intStepC p (GCall gname (Ctr cname cargs : args)) =
+	(body // zip (cvs ++ vs) (cargs ++ args), 1) where
+		(GDef _ (Pat _ cvs) vs body) = gDef p gname cname
+
+intStepC p (GCall gname (e:es)) =
+	(GCall gname (steppedE : es), c) where
+    (steppedE, c) = intStepC p e
