a5ce42817c938b42f9d5f05283580dba0856b800
Podkopaev Anton <podkoav239@gmail.com>
2014-02-05 11:46:35 +0400

    Added tests.hs

--- Interpreters/BigStepCBN_Maybe.hs
+++ Interpreters/BigStepCBN_Maybe.hs
@@ -10,11 +10,11 @@ int p (Atom a) = Just $ Atom a
 
 int p (Ctr name args) | l <- map (int p) args, all isJust l = Just $ Ctr name $ catMaybes l
 
-int p (FCall name args) | ((FDef _ vs body):xs) <- fDefs p name, length args == length vs =
+int p (FCall name args) | ((FDef _ vs body):_) <- fDefs p name, length args == length vs =
                           	int p (body // zip vs args)
 
 int p (GCall gname (arg0:args)) | Just (Ctr cname cargs) <- int p arg0
-                                , ((GDef _ (Pat _ cvs) vs body):xs) <- gDefsPat p gname cname 
+                                , ((GDef _ (Pat _ cvs) vs body):_) <- gDefsPat p gname cname 
                                 , length cvs + length vs == length cargs + length args =
                                 	int p (body // zip (cvs ++ vs) (cargs ++ args))
 
---
+++ tests.hs
@@ -0,0 +1,8 @@
+module Test where
+
+import Data
+import DataIO
+import Examples.Examples
+
+--import Interpreters.BigStepCBN_Maybe
+import Interpreters.BigStepCBN_ND
